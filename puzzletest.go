e1ccb8a6b56d2794a01a693a16b6ff440932a827
dvaumoron <dvaumoron@gmail.com>
2022-12-13 22:35:50 +0100

    adding wikiGroup

--- puzzletest.go
+++ puzzletest.go
@@ -19,19 +19,31 @@ package main
 
 import (
 	"github.com/dvaumoron/puzzleweb"
+	"github.com/dvaumoron/puzzleweb/admin"
+	"github.com/dvaumoron/puzzleweb/admin/client"
 	"github.com/dvaumoron/puzzleweb/locale"
 	"github.com/dvaumoron/puzzleweb/login"
 	"github.com/dvaumoron/puzzleweb/wiki"
 	"golang.org/x/text/language"
 )
 
+const wikiGroup1Id = 2
+const wikiGroup2Id = 3
+
 func main() {
 	locale.Availables.Add(language.French)
+	locale.Availables.Add(language.English)
+
+	client.RegisterGroup(wikiGroup1Id, "wiki.group1")
+	client.RegisterGroup(wikiGroup2Id, "wiki.group2")
 
 	site := puzzleweb.CreateSite()
 
 	login.AddLoginPage(site, "login")
-	site.AddPage(wiki.NewWikiPage("wiki", 2))
+	admin.AddAdminPage(site, "admin")
+	site.AddPage(wiki.NewWikiPage("wiki", wikiGroup1Id, 1))
+	site.AddPage(wiki.NewWikiPage("wiki2", wikiGroup1Id, 2))
+	site.AddPage(wiki.NewWikiPage("wiki3", wikiGroup2Id, 3))
 	// TODO
 
 	site.Run()
