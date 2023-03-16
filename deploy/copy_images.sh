167770a29a75a135452b66b723de90b255482de7
dvaumoron <dvaumoron@gmail.com>
2023-03-16 18:26:57 +0100

    improve copy_images.sh

--- deploy/copy_images.sh
+++ deploy/copy_images.sh
@@ -12,3 +12,16 @@ buildah push puzzleprofileserver docker-daemon:puzzleprofileserver:latest
 buildah push puzzleblogserver docker-daemon:puzzleblogserver:latest
 buildah push puzzlewikiserver docker-daemon:puzzlewikiserver:latest
 buildah push puzzleforumserver docker-daemon:puzzleforumserver:latest
+
+k3d image import puzzletest -c puzzlecluster -t
+k3d image import puzzlesessionserver -c puzzlecluster -t
+k3d image import puzzlerightserver -c puzzlecluster -t
+k3d image import puzzlecachedrightserver -c puzzlecluster -t
+k3d image import puzzlesettingsserver -c puzzlecluster -t
+k3d image import puzzleloginserver -c puzzlecluster -t
+k3d image import puzzlesaltserver -c puzzlecluster -t
+k3d image import puzzlemarkdownserver -c puzzlecluster -t
+k3d image import puzzleprofileserver -c puzzlecluster -t
+k3d image import puzzleblogserver -c puzzlecluster -t
+k3d image import puzzlewikiserver -c puzzlecluster -t
+k3d image import puzzleforumserver -c puzzlecluster -t
