f363d7f74d945403948df7868e6704d71771b323
dvaumoron <dvaumoron@gmail.com>
2023-03-17 11:32:19 +0100

    fix create_fake_cluster.sh

--- deploy/create_fake_cluster.sh
+++ deploy/create_fake_cluster.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 # ensure there is no remaining puzzlecluster in k3d
-helm delete istio-ingress -n istio-system
-helm delete istiod -n istio-system
-helm delete istio-base -n istio-system
 k3d cluster delete puzzlecluster
 
 # create fake cluster with k3d
-k3d cluster create puzzlecluster --servers 3 --api-port 6550 -p "9080:80@loadbalancer"  -p "9443:443@loadbalancer" --agents 2 --k3s-arg '--disable=traefik@server:*'
+k3d cluster create puzzlecluster --servers 3 --api-port 6550 -p "9080:80@loadbalancer" --agents 1 --k3s-arg '--disable=traefik@server:*'
 
 # set up Istio
 kubectl create namespace istio-system
@@ -16,4 +13,4 @@ helm install istiod istio/istiod -n istio-system --wait
 
 # install ingress gateway
 kubectl label namespace istio-system istio-injection=enabled
-helm install istio-ingress istio/gateway -n istio-system --wait
+helm install istio-ingressgateway istio/gateway -n istio-system --wait
