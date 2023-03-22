72e84c52087e443a8cab0db0530a1a74762eeddb
dvaumoron <dvaumoron@gmail.com>
2023-03-22 10:14:48 +0100

    working on k8s conf

--- build/build_image.sh
+++ build/build_image.sh
@@ -8,7 +8,8 @@ buildah copy puzzletest-working-container ./locales /locales
 buildah copy puzzletest-working-container ./static /static
 buildah copy puzzletest-working-container ./templates /templates
 buildah copy puzzletest-working-container $HOME/go/bin/puzzletest /bin/puzzletest
-buildah config --port 8080 puzzletest-working-container
+buildah config --env SITE_PORT=9080 puzzletest-working-container
+buildah config --port 9080 puzzletest-working-container
 buildah config --entrypoint '["/bin/puzzletest"]' puzzletest-working-container
 buildah commit puzzletest-working-container puzzletest
 buildah rm puzzletest-working-container
--- deploy/create_fake_cluster.sh
+++ deploy/create_fake_cluster.sh
@@ -4,6 +4,6 @@
 k3d cluster delete puzzlecluster
 
 # create fake cluster with k3d
-k3d cluster create puzzlecluster --servers 3 --api-port 6550 -p "8080:8080@loadbalancer" --agents 1
+k3d cluster create puzzlecluster --servers 3 --api-port 6550 -p "9080:80@loadbalancer" --agents 1
 
 ./deploy/copy_images.sh
