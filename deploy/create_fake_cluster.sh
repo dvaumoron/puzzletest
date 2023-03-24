746a2248c94cce08b8364dfeca9f4194f5a2c275
dvaumoron <dvaumoron@gmail.com>
2023-03-24 16:34:10 +0100

    fix

--- deploy/create_fake_cluster.sh
+++ deploy/create_fake_cluster.sh
@@ -4,6 +4,7 @@
 k3d cluster delete puzzlecluster
 
 # create fake cluster with k3d
-k3d cluster create puzzlecluster --servers 3 --api-port 6550 -p "9080:80@loadbalancer" --agents 1
+export K3D_FIX_DNS=1
+k3d cluster create puzzlecluster --api-port 6550 --servers 3 --agents 1 -p "9080:80@loadbalancer:*"
 
 ./deploy/copy_images.sh
