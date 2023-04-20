61b2b7b783753abb85e08aadfd6610968a1d4ddd
dvaumoron <dvaumoron@gmail.com>
2023-04-20 14:28:35 +0200

    update script

--- deploy/copy_images.sh
+++ deploy/copy_images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-kind load docker-image puzzleproxy -n puzzlecluster
 kind load docker-image puzzletest -n puzzlecluster
 kind load docker-image puzzlesessionserver -n puzzlecluster
 kind load docker-image puzzlerightserver -n puzzlecluster
@@ -13,3 +12,4 @@ kind load docker-image puzzleprofileserver -n puzzlecluster
 kind load docker-image puzzleblogserver -n puzzlecluster
 kind load docker-image puzzlewikiserver -n puzzlecluster
 kind load docker-image puzzleforumserver -n puzzlecluster
+kind load docker-image puzzlepassstrengthserver -n puzzlecluster
