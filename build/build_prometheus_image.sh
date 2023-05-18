99a3a8d7f3575b6ff302e7a282555657b71bbf4e
dvaumoron <dvaumoron@gmail.com>
2023-05-18 14:50:20 +0200

    script (telemetry)

---
+++ build/build_prometheus_image.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+buildah from --name prometheus-working-container scratch
+buildah copy prometheus-working-container ./prometheus/prometheus /
+buildah copy prometheus-working-container ./prometheus/promtool /
+buildah copy prometheus-working-container ./prometheus/consoles /consoles
+buildah copy prometheus-working-container ./prometheus/console_libraries /console_libraries
+buildah copy prometheus-working-container $HOME/go/bin/puzzletest /bin/puzzletest
+buildah config --port 9090 prometheus-working-container
+buildah config --entrypoint '["/prometheus", "--config.file=prometheus.yaml"]' prometheus-working-container
+buildah commit prometheus-working-container prometheus
+buildah rm prometheus-working-container
+
+buildah push prometheus docker-daemon:prometheus:latest
