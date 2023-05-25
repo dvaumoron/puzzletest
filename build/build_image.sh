4b85dc2d1d203cd5393eecdcd3189f5dbe5d3c17
dvaumoron <dvaumoron@gmail.com>
2023-05-25 13:39:55 +0200

    fix script (no more template here)

--- build/build_image.sh
+++ build/build_image.sh
@@ -7,7 +7,6 @@ puzzletools prepare
 buildah from --name puzzletest-working-container scratch
 buildah copy puzzletest-working-container ./locales /locales
 buildah copy puzzletest-working-container ./static /static
-buildah copy puzzletest-working-container ./templates /templates
 buildah copy puzzletest-working-container $HOME/go/bin/puzzletest /bin/puzzletest
 buildah config --env SITE_PORT=9080 puzzletest-working-container
 buildah config --port 9080 puzzletest-working-container
