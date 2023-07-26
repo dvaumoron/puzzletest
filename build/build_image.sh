4db1a6afc9b81cd1d6d4768dc695f95f37ff8461
dvaumoron <dvaumoron@gmail.com>
2023-07-26 18:18:08 +0200

    update build_image.sh

--- build/build_image.sh
+++ build/build_image.sh
@@ -2,10 +2,7 @@
 
 ./build/build.sh
 
-puzzletools prepare
-
 buildah from --name puzzletest-working-container scratch
-buildah copy puzzletest-working-container ./locales /locales
 buildah copy puzzletest-working-container ./static /static
 buildah copy puzzletest-working-container $HOME/go/bin/puzzletest /bin/puzzletest
 buildah config --env SITE_PORT=9080 puzzletest-working-container
