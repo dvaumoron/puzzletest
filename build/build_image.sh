8d48fe9d07dac01406572e46de336d8ceb1c267b
dvaumoron <dvaumoron@gmail.com>
2023-10-24 11:53:57 +0200

    update helm chart

--- build/build_image.sh
+++ build/build_image.sh
@@ -4,8 +4,10 @@
 
 buildah from --name puzzleweb-working-container scratch
 buildah copy puzzleweb-working-container ./static /static
+# frame.yaml must be compatible with the deployment environment (beware of serviceAddr for custom widgets in widgets part)
 buildah copy puzzleweb-working-container ./frame.yaml /frame.yaml
 buildah copy puzzleweb-working-container $HOME/go/bin/puzzleweb /bin/puzzleweb
+buildah config --env FRAME_CONFIG_PATH=frame.yaml puzzleweb-working-container
 buildah config --env SITE_PORT=9080 puzzleweb-working-container
 buildah config --port 9080 puzzleweb-working-container
 buildah config --entrypoint '["/bin/puzzleweb"]' puzzleweb-working-container
